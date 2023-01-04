92bf7d3e6ec01f2096b3ec5f436e487f23f80800
Benny Powers <web@bennypowers.com>
2023-01-04 08:54:15 +0200

    fix(web-dev-server): bust cache

--- packages/web-dev-server-plugin-lit-css/web-dev-server-plugin-lit-css.ts
+++ packages/web-dev-server-plugin-lit-css/web-dev-server-plugin-lit-css.ts
@@ -1,29 +1,54 @@
 import type { Options } from '@pwrs/lit-css/lit-css';
 import type { Plugin } from '@web/dev-server-core';
 
-import { litCss as litCssRollup } from 'rollup-plugin-lit-css';
+import { transform } from '@pwrs/lit-css';
 import { createFilter } from '@rollup/pluginutils';
-
-import { fromRollup } from '@web/dev-server-rollup';
 
 export interface LitCSSOptions extends Omit<Options, 'css'> {
   include?: RegExp | string[];
   exclude?: RegExp | string[];
 }
 
-const rollupPluginLitCss = fromRollup(litCssRollup);
-
 export function litCss(options?: LitCSSOptions): Plugin {
-  const filter = createFilter(options?.include ?? /\.css$/i, options?.exclude);
+  const {
+    exclude,
+    include = /\.css$/i,
+    uglify,
+    specifier,
+    tag,
+    ...rest
+  } = options ?? {};
+
+  const filter = createFilter(include, exclude);
 
   return {
-    ...rollupPluginLitCss(options),
     name: 'lit-css',
-    resolveMimeType(context) {
-      if (filter(context.path))
-        return 'js';
+
+    async transform(ctx) {
+      const { path, body } = ctx;
+      if (filter(path)) {
+        // bust the cache
+        ctx.set('Cache-Control', 'no-cache');
+        ctx.set('ETag', Date.now().toString());
+        ctx.set('Last-Modified', new Date().toString());
+        return transform({
+          css: body as string,
+          specifier,
+          tag,
+          uglify,
+          filePath: path,
+          ...rest
+        });
+      }
     },
+
+    resolveMimeType({ path }) {
+      if (filter(path))
+        return 'js';
+    }
   };
 }
 
 export default litCss;
+
+
